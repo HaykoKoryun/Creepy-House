496743d68da068e1d68e0061c14f048ee140a116
Hayk Galstyan <haykokoryun@gmail.com>
2019-01-26 19:31:08 +0400

    drinks list

---
+++ src/data/drinks.js
@@ -0,0 +1,43 @@
+const drinks =
+[ { name: 'Godfather'
+  , image: 'drink'
+  }
+, { name: 'Rob Roy'
+  , image: 'drink'
+  }
+, { name: 'Hanky Panky'
+  , image: 'drink'
+  }
+, { name: 'Jungle Bird'
+  , image: 'drink'
+  }
+, { name: 'White Russian'
+  , image: 'drink'
+  }
+, { name: 'Painkiller'
+  , image: 'drink'
+  }
+, { name: 'Sidecard'
+  , image: 'drink'
+  }
+, { name: 'Paloma'
+  , image: 'drink'
+  }
+, { name: 'French 75'
+  , image: 'drink'
+  }
+, { name: 'Corpse Reviver'
+  , image: 'drink'
+  }
+, { name: 'Zombie'
+  , image: 'drink'
+  }
+, { name: 'Sazerac'
+  , image: 'drink'
+  }
+, { name: 'Stalin'
+  , image: 'drink'
+  }
+];
+
+export default drinks;
