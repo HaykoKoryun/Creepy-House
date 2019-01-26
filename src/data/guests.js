8f0d357ae8c757c013231e011b2cf0664313fe57
Hayk Galstyan <haykokoryun@gmail.com>
2019-01-26 19:31:13 +0400

    guests list

---
+++ src/data/guests.js
@@ -0,0 +1,40 @@
+const guests =
+[ { image: 'babushka'
+  , gender: 'female' 
+  }
+, { image: 'babushka'
+  , gender: 'female'
+}
+, { image: 'babushka'
+  , gender: 'female' 
+  }
+, { image: 'babushka'
+  , gender: 'female'
+  }
+, { image: 'babushka'
+  , gender: 'female' 
+  }
+, { image: 'zombie'
+  , gender: 'female'
+  }
+, { image: 'zombie'
+  , gender: 'female' 
+  }
+, { image: 'zombie'
+  , gender: 'male'
+  }
+, { image: 'zombie'
+  , gender: 'male' 
+  }
+, { image: 'zombie'
+  , gender: 'male'
+  }
+, { image: 'zombie'
+  , gender: 'male' 
+  }
+, { image: 'zombie'
+  , gender: 'male'
+  }
+];
+
+export default guests;
