af481df1e124cb4789f4e0f2822cb4c6a4db1d4d
Hayk Galstyan <haykokoryun@gmail.com>
2019-01-26 13:19:49 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div
+      id="bg"
+      :class="{blur:blurBackground}"
+    ></div>
+    <MainMenu
+      
+    ></MainMenu>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import MainMenu from './components/MainMenu.vue';
+
+export default
+{ name: 'app'
+, components:
+  { MainMenu
+  }
+, computed: mapState(
+  { blurBackground: 'blurBackground'
+  })
+}
+</script>
+
+<style>
+#app
+{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+
+#bg
+{
+  position: absolute;
+  left: -1%;
+  top: -1%;
+  width: 102%;
+  height: 102%;
+  background-image: url(./assets/images/bg.jpg);
+  background-size: cover;
+  background-position: 50%;
+}
+
+#bg.blur
+{
+  filter: blur(10px);
+}
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="main-menu">
+    <h1>Party 'til Dawn</h1>
+    <div
+      id="start"
+      :on-click="$emit('start')"
+    >
+      START
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainMenu'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #main-menu
+  {
+    position: absolute;
+    font-family: 'Rock Salt', cursive;
+    width: 500px;
+    height: 500px;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    margin: auto;
+  }
+
+  h1
+  {
+    color: #fff;
+    font-size: 80px;
+    line-height: 100px;
+  }
+
+  #start
+  {
+    font-size: 40px;
+    font-weight: bold;
+    color: #f0f;
+    letter-spacing: 10px;
+  }
+</style>
