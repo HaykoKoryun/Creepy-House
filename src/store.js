13ac9b25f2bb8d17f10899775d1c24fced354a2a
Hayk Galstyan <haykokoryun@gmail.com>
2019-01-27 14:32:52 +0400

    start properly

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state:
   { // TODO: change to false when finished
-    started: true
+    started: false
   , dialog: ''
   , drinks: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   , guests: [new Guest(1),new Guest(1),new Guest(1),new Guest(1),new Guest(2),new Guest(2),new Guest(2),new Guest(3)]
