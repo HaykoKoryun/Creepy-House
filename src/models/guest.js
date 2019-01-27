abd2f459608077336a931916c78cf8d0886ab9b5
JNCMark <amiraghyan03@gmail.com>
2019-01-27 12:37:38 +0400

    fix.

--- src/models/guest.js
+++ src/models/guest.js
@@ -73,7 +73,7 @@ export default class Guest {
             this.chooseDrinks();
             
             this.level++;
-            processLevel(this.level);
+            this.processLevel(this.level);
             this.stage = 1;
             return this.activate();
         }
